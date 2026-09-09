7c530035a7cf5479ce173f508f0b06e5b7f07c95
echalmers <echalmers@mtroyal.ca>
2026-09-09 13:04:29 -0600

    initial commit

---
+++ hello_world/main.py
@@ -0,0 +1,21 @@
+from environment import MiningGame
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# instantiate the game
+game = MiningGame(n_mines=2)
+cumulative_reward = 0
+
+# repeatedly ask user to choose a place to mine, and execute their choice
+for step in range(150):
+    choice = input("choose a place to mine (0 or 1). q to quit: ")
+    if choice == 'q':
+        break
+    
+    reward = game.choose_mine(int(choice))
+    cumulative_reward += reward
+    print(f"reward: {reward}, cumulative reward: {cumulative_reward}")
+    
+# print the true reward probabilities for mines 0 and 1
+print('reward probabilities for mine 0/1 were:', game.reward_probabilities)
